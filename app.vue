354593b8fc1bf6ef42c7731f02921d66a79ded24
dibunbuns <bunniequeenrules@gmail.com>
2025-05-25 23:21:36 -0400

    reactivity achieved

--- app.vue
+++ app.vue
@@ -1,6 +1,15 @@
+<script setup>
+const input = ref('empty');
+
+function submit(){
+  alert(input.value);
+}
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <input type="text" v-model="input">
+    <p>{{ input }}</p>
+    <button @click="submit">submit</button>
   </div>
 </template>
