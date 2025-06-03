9557203045f43c01497116299a5399cab1f7ff5e
dibunbuns <bunniequeenrules@gmail.com>
2025-06-02 23:35:51 -0400

    target id on edit click

--- app.vue
+++ app.vue
@@ -1,19 +1,30 @@
 <script setup lang="ts">
 const todoStore = useTodoStore();
-const input = ref('laundry....');
+const input = ref("laundry....");
+const edit = ref('');
+const editField = ref(false);
 
+function enableEdit(id: string) {
+  document.getElementById(id)!.disabled = false;
+}
 </script>
 
 <template>
   <div>
-
-    <input type="text" v-model="input">
-
+    <input type="text" v-model="input" />
     <button @click="todoStore.addItem(input)">submit</button>
+
     <p v-for="item in todoStore.todoList" :key="item.id">
-      {{ item.id }}, {{ item.name }}
+      <input :id="item.id" disabled :value="item.name" />
       <button @click="todoStore.deleteItem(item.id)">delete</button>
-      <button>edit</button>
+      <button
+        @click="
+          todoStore.editItem(item.id, edit);
+          enableEdit(item.id);
+        "
+      >
+        edit
+      </button>
     </p>
   </div>
 </template>
--- stores/todo.ts
+++ stores/todo.ts
@@ -1,25 +1,33 @@
-export const useTodoStore = defineStore('todo', () => {
-    type TodoItem = {
-        id: string,
-        name: string
-    }
+export const useTodoStore = defineStore("todo", () => {
+  type TodoItem = {
+    id: string;
+    name: string;
+  };
 
-    const todoList = ref<TodoItem[]>([]);
+  const todoList = ref<TodoItem[]>([]);
 
-    function addItem(name: string) {
-        const uuid = self.crypto.randomUUID();
-        const item: TodoItem = {id: uuid, name: name};
-        todoList.value.push(item);
-    }
+  function addItem(name: string) {
+    const uuid = self.crypto.randomUUID();
+    const item: TodoItem = { id: uuid, name: name };
+    todoList.value.push(item);
+  }
 
-    function deleteItem(id: string){
-        todoList.value = todoList.value.filter((item) => item.id != id)
-        // grabs everything in array that doesn't match id, then reassigns those items to the todolist
-    }
+  function deleteItem(id: string) {
+    todoList.value = todoList.value.filter((item) => item.id != id);
+    // grabs everything in array that doesn't match id, then reassigns those items to the todolist
+  }
 
-    return {
-        todoList,
-        addItem,
-        deleteItem
-    }
+  function editItem(id: string, edit: string) {
+    let todoItem = todoList.value.find((item) => item.id === id)
+    console.log(todoItem)
+    let newItem = "hi";
+    // goal: in place editing
+  }
+
+  return {
+    todoList,
+    addItem,
+    deleteItem,
+    editItem,
+  };
 });
